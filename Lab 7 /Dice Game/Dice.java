97bd4a1072474d4922a94ebf38cf2bc175bff830
gilbertoHennepin <155484528+gilbertoHennepin@users.noreply.github.com>
2025-04-09 04:16:50 -0500

    Create Dice.java

---
+++ Lab 7 /Dice Game/Dice.java
@@ -0,0 +1,11 @@
+package week7.knockout_game;
+
+import java.util.Random;
+
+public class Dice {
+
+    public int roll() {
+        Random rnd = new Random();
+        return rnd.nextInt(6) + 1;  // add 1, range between 1 and 6
+    }
+}
