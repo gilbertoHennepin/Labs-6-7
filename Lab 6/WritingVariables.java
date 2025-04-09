120be5e52ad7c58688f7e0d1304cca3dd66158ec
gilbertoHennepin <155484528+gilbertoHennepin@users.noreply.github.com>
2025-04-08 23:18:17 -0500

    Create WritingVariables.java

---
+++ Lab 6/WritingVariables.java
@@ -0,0 +1,25 @@
+package week6;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class WritingVariables {
+    public static void main(String[] args) throws IOException {
+
+        int classCode = 2454;
+        double averageEnrollment = 22.16;   // average number of students in a semester
+        String className = "Java";
+
+        FileWriter writer = new FileWriter("java.txt");
+        BufferedWriter bufferedWriter = new BufferedWriter(writer);
+
+        bufferedWriter.write(classCode + "\n");
+        bufferedWriter.write(averageEnrollment + "\n");
+        bufferedWriter.write(className + "\n");
+
+        bufferedWriter.close();
+        writer.close();
+
+    }
+}
